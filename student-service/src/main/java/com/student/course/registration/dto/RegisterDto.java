28b6d7827cd3892603aa106c57170d155c00cfc4
brkybzkrt <b9bozkurt959@gmail.com>
2025-06-19 19:35:31 +0300

    student-service was updated, login and register functions were implemented

--- student-service/src/main/java/com/student/course/registration/dto/RegisterDto.java
+++ student-service/src/main/java/com/student/course/registration/dto/RegisterDto.java
@@ -1,4 +1,22 @@
 package com.student.course.registration.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class RegisterDto {
+
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private String username;
+
+    @Size(min = 7,max = 14)
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email format must be right")
+    private String email;
 }
--- student-service/src/main/java/com/student/course/registration/dto/StudentResponseDto.java
+++ student-service/src/main/java/com/student/course/registration/dto/StudentResponseDto.java
@@ -1,4 +1,19 @@
 package com.student.course.registration.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentResponseDto {
+
+    private Long id;
+    private String username;
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
