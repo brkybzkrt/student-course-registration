55bac41a4421e8dbcd8577d836b3ae62cf5a7089
brkybzkrt <b9bozkurt959@gmail.com>
2025-06-21 00:05:07 +0300

    AdminController was updated

--- admin-service/src/main/java/com/student/course/registration/controller/impl/AdminController.java
+++ admin-service/src/main/java/com/student/course/registration/controller/impl/AdminController.java
@@ -8,10 +8,7 @@ import com.student.course.registration.service.IAdminService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/admins")
@@ -20,31 +17,38 @@ public class AdminController implements IAdminController {
     @Autowired
     private IAdminService adminService;
 
+    @PostMapping("/login")
     @Override
     public ResponseEntity<Object> login(@RequestBody @Valid LoginDto loginDto) {
         return adminService.login(loginDto);
     }
 
+    @PostMapping("/register")
     @Override
     public ResponseEntity<Object> register(@RequestBody @Valid RegisterDto registerDto) {
         return adminService.register(registerDto);
     }
 
+    @PostMapping
     @Override
     public ResponseEntity<Object> create(@RequestBody @Valid AdminCreateUpdateDto createDto) {
         return adminService.create(createDto);
     }
 
+    @GetMapping("/{id}")
     @Override
     public ResponseEntity<Object> getById(@RequestParam(name = "id") String id) {
         return adminService.getById(id);
     }
 
+
+    @PutMapping("/{id}")
     @Override
     public ResponseEntity<Object> updateById(@RequestParam(name = "id") String id,@RequestBody @Valid AdminCreateUpdateDto adminCreateUpdateDto) {
         return adminService.updateById(id,adminCreateUpdateDto);
     }
 
+    @DeleteMapping("/{id}")
     @Override
     public ResponseEntity<Object> deleteById(@RequestParam(name = "id") String id) {
         return adminService.deleteById(id);
